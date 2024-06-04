f5dd5ff6036c6875d89713e8b065f6f6b286aa91
Florian LOTTE <florian.lotte@gmail.com>
2024-06-04 23:52:15 +0200

    commit chapter 12
    https://nextjs.org/learn/dashboard-app/error-handling

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,12 +16,12 @@ const pool = new Pool({ connectionString: process.env.POSTGRES_URL });
 type Primitive = string | number | boolean | undefined | null;
 
 function sqlTemplate(
-    strings: TemplateStringsArray,
-    ...values: Primitive[]
+  strings: TemplateStringsArray,
+  ...values: Primitive[]
 ): [string, Primitive[]] {
   if (!isTemplateStringsArray(strings) || !Array.isArray(values)) {
     throw new Error(
-        'It looks like you tried to call `sql` as a function. Make sure to use it as a tagged template.\n' +
+      'It looks like you tried to call `sql` as a function. Make sure to use it as a tagged template.\n' +
         "\tExample: sql`SELECT * FROM users`, not sql('SELECT * FROM users')",
     );
   }
@@ -36,16 +36,16 @@ function sqlTemplate(
 }
 
 function isTemplateStringsArray(
-    strings: unknown,
+  strings: unknown,
 ): strings is TemplateStringsArray {
   return (
-      Array.isArray(strings) && 'raw' in strings && Array.isArray(strings.raw)
+    Array.isArray(strings) && 'raw' in strings && Array.isArray(strings.raw)
   );
 }
 
 async function sql<O extends QueryResultRow>(
-    strings: TemplateStringsArray,
-    ...values: Primitive[]
+  strings: TemplateStringsArray,
+  ...values: Primitive[]
 ) {
   const [query, params] = sqlTemplate(strings, ...values);
 
--- scripts/seed.js
+++ scripts/seed.js
@@ -175,17 +175,19 @@ async function main() {
 
   const values = (values, { columns = Object.keys(values) } = {}) => {
     if (!Array.isArray(values)) {
-      values = columns.map(column => values[column]);
+      values = columns.map((column) => values[column]);
     }
-    return valuePosition => ({
-      text: Array.apply(null, { length: values.length }).map(() => '$' + (++valuePosition)).join(', '),
-      values
-    })
+    return (valuePosition) => ({
+      text: Array.apply(null, { length: values.length })
+        .map(() => '$' + ++valuePosition)
+        .join(', '),
+      values,
+    });
   };
   client.sql = (textFragments, ...valueFragments) => {
     const query = {
       text: textFragments[0],
-      values: []
+      values: [],
     };
     valueFragments.forEach((valueFragment, i) => {
       if (typeof valueFragment !== 'function') {
